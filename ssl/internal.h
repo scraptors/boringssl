2a3ed2db5d83c4b37b296771a9f35898f5dbaa8a
Samuel Cobb <sjcobb2003@gmail.com>
2026-02-06 02:35:12 +0000

    move for merge

--- ssl/internal.h
+++ ssl/internal.h
@@ -4051,13 +4051,13 @@ struct ssl_ctx_st : public bssl::RefCounted<ssl_ctx_st> {
 
   // grease_enabled is whether GREASE (RFC 8701) is enabled.
   bool grease_enabled : 1;
-
-  // permute_extensions is whether to permute extensions when sending messages.
-  bool permute_extensions : 1;
-
+  
   // rama_ssl_extension_order, if not empty, will use this actions
   // as the order to be used to write the ssl extensions.
   bssl::Array<uint16_t> extension_order;
+
+  // permute_extensions is whether to permute extensions when sending messages.
+  bool permute_extensions : 1;
 
   // allow_unknown_alpn_protos is whether the client allows unsolicited ALPN
   // protocols from the peer.
